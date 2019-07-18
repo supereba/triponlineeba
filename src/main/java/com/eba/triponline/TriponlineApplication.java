6a3cc8667536def88e24d5c9c327413a916074c6
Emmanuel Brun D'Aubignosc <emmanuel@brun>
2019-07-18 11:41:38 +0200

    Commit initial du back

---
+++ src/main/java/com/eba/triponline/TriponlineApplication.java
@@ -0,0 +1,13 @@
+package com.eba.triponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriponlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriponlineApplication.class, args);
+    }
+
+}
